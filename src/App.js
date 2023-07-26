4bcf81b5a9efe6c045b10ced50b4ee71e3d5762e
Bastien Andre <bastienandre68@gmail.com>
2023-07-26 23:52:00 +0200

    commit firsted

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload coucou.
+          Edit <code>src/App.js</code> and save to reload coucou ça va.
         </p>
         <a
           className="App-link"
